19520cd0888f86e6ed0c127444183ffbf64148cd
Ma-minghao <2788808114@qq.com>
2023-12-09 19:00:01 +0800

    2rd

--- test/testGorm.go
+++ test/testGorm.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"ginchat/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	// 设置数据库连接参数，包括 SSL 配置
-	dsn := "root:180319@tcp(127.0.0.1:3306)/forlearning?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:180319@tcp(127.0.0.1:3306)/my_database?charset=utf8mb4&parseTime=True&loc=Local"
 	// 连接数据库
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -17,14 +18,14 @@ func main() {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&models.UserBasic{})
-
-	// Create
-	user := &models.UserBasic{}
-
-	db.Create(user)
+	//db.AutoMigrate(&models.UserBasic{})
 
 	// Read
-	fmt.Println(db.First(user,1))
+	user := &models.UserBasic{}
+
+	//Create
+	//db.Create(user)
+
+	fmt.Println(db.First(user, 1))
 	db.Model(user).Update("Password", "180319")
 }
